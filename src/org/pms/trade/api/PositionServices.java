89015d46b2a18ac4977d643f62e204940b15c300
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-04-27 17:23:28 +0800

    getPosition

--- src/org/pms/trade/api/PositionServices.java
+++ src/org/pms/trade/api/PositionServices.java
@@ -2,10 +2,9 @@ package org.pms.trade.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.pms.trade.entity.PositionEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "PositionServices")
 @RestController
@@ -16,4 +15,8 @@ public interface PositionServices {
     @Operation(summary = "savePosition", description = "")
     public Boolean savePosition(
             @RequestHeader("transactionRef") String transactionRef, @RequestHeader("instrumentId") String instrumentId, @RequestHeader("tradeType") String tradeType, @RequestHeader("unit") Integer unit);
+
+    @GetMapping("/getPosition")
+    public PositionEntity getPosition(@RequestParam("transactionRef") String transactionRef);
+
 }
--- src/org/pms/trade/api/PositionServicesImpl.java
+++ src/org/pms/trade/api/PositionServicesImpl.java
@@ -5,6 +5,7 @@ import org.pms.trade.entity.PositionEntity;
 import org.pms.trade.entity.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
@@ -33,5 +34,23 @@ public class PositionServicesImpl implements PositionServices {
         }
 
         return ret;
+    }
+
+    public PositionEntity getPosition(@RequestParam("transactionRef") String transactionRef) {
+        PositionEntity positionEntity = new PositionEntity();
+
+        try {
+            positionEntity.setId(UUID.randomUUID().toString());
+            positionEntity.setTransaction_ref("123");
+            positionEntity.setInstrument_Id("123456");
+            positionEntity.setTrade_type("buy");
+            positionEntity.setUnit(1);
+            positionService.savePosition(positionEntity);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return positionEntity;
     }
 }
